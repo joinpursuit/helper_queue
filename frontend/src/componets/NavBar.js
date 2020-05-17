4770e238235a651ead4656cb4ac68ffc3ee67975
Corey Ladovsky <coreyskymusic@gmail.com>
2020-05-17 18:30:27 -0400

    navbar styles

--- frontend/src/componets/NavBar.js
+++ frontend/src/componets/NavBar.js
@@ -3,19 +3,18 @@ import { NavLink } from 'react-router-dom';
 import "../css/NavBar.css";
 import { AuthContext } from '../providers/AuthProvider';
 import { logout } from '../util/firebaseFunctions';
+import { useLocation } from 'react-router-dom'
 
-export default function NavBar(params) {
+export default function NavBar() {
     const { currentUser } = useContext(AuthContext);
+    const location = useLocation();
     const display = () => {
         if(currentUser) {
             return <button className="logoutButton" onClick={logout}>Log Out</button>;
+        } else if(location.pathname === "/login") {
+            return  <NavLink className="signUpAndLogin" to={"/signup"}>Sign Up</NavLink>
         } else {
-            return (
-              <>
-                <NavLink to={"/signup"}>Sign Up</NavLink>
-                <NavLink to={"/login"}>Log In</NavLink>
-              </>
-            );
+           return <NavLink to={"/login"} className="signUpAndLogin" >Login</NavLink>
         }
     }
     return(
