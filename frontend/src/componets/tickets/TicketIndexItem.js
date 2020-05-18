3853661b430aa1900273728db0891926b6c2e9fb
Corey Ladovsky <coreyskymusic@gmail.com>
2020-05-17 21:46:02 -0400

    added running time waited

--- frontend/src/componets/tickets/TicketIndexItem.js
+++ frontend/src/componets/tickets/TicketIndexItem.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import TimeAgo from 'react-timeago';
 
-export default function TicketIndexItem({ticket, removeTicket}) {
+ export default function TicketIndexItem({ticket, removeTicket}) {
+
     return(
         <li className="ticketIndexItem">
         <div>
             <p> {ticket.email} </p>
             <button onClick={() => removeTicket(ticket.id)}>Complete</button>
+            <TimeAgo date={ticket.created_at} />
         </div>
             
         </li>
