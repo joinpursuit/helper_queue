ea31fdc403a7529e490d19ae6d493890594bb7d1
Corey Ladovsky <coreyskymusic@gmail.com>
2020-07-14 21:34:57 -0400

    migration5

---
+++ backend/db/migrations/migration5.sql
@@ -0,0 +1,16 @@
+
+-- \c helper_queue;
+
+CREATE TABLE jobs_status_timelines
+(
+    id SERIAL PRIMARY KEY,
+    status VARCHAR,
+    created_at TIMESTAMP WITH TIME ZONE,
+    job_id INT REFERENCES jobs(id) ON DELETE SET NULL 
+    );
+
+
+INSERT INTO jobs_status_timelines (status, created_at, job_id) SELECT status, created_at, id FROM jobs;
+
+
+ALTER TABLE jobs_status_timelines ALTER created_at SET DEFAULT CURRENT_TIMESTAMP;
