b94a8ef9f90e6c1c676caee380f4cf53182c0a12
Corey Ladovsky <coreyskymusic@gmail.com>
2020-07-28 19:16:17 -0400

    mores tests RequestHelp

--- frontend/src/componets/RequestHelp.js
+++ frontend/src/componets/RequestHelp.js
@@ -1,91 +1,92 @@
-import React, { useState, useContext, useEffect } from 'react';
-import axios from 'axios';
-import { apiURL } from '../util/apiURL';
+import React, { useState, useContext, useEffect } from "react";
+import axios from "axios";
+import { apiURL } from "../util/apiURL";
 import { AuthContext } from "../providers/AuthProvider";
 import socketIOClient from "socket.io-client";
 import "../css/RequestHelp.css";
 
 export default function RequestHelp() {
-    const [isWaitingForHelp, setIsWaitingForHelp] = useState(false);
-    const [openTicket, setOpenTicket] = useState(null)
-    const { token, currentUser } = useContext(AuthContext);
-    const API = apiURL();
-    const socket = socketIOClient(API);
-    const fetchOpenTicket = async () => {
-        try {
-            let res = await axios({
-                method: 'get', 
-                url: `${API}/api/tickets/open_tickets/`,
-                headers: {
-                    'AuthToken': token
-                }
-            })
-            if(res.data.openTicket.length) {
-                setIsWaitingForHelp(true);
-                setOpenTicket(res.data.openTicket[0])
-            } else {
-                setIsWaitingForHelp(false);
-                setOpenTicket(null)
-            }    
-        } catch (error) {
-            setIsWaitingForHelp(false);
-            setOpenTicket(null)
-        }
-        
-    }
-    useEffect(() => {
-        fetchOpenTicket()
-    }, [])
-    
-    useEffect(() => {
-        socket.on("ticketClose", fetchOpenTicket)
-        return () => socket.off("ticketClose", fetchOpenTicket)
-    }, [])
+  const [isWaitingForHelp, setIsWaitingForHelp] = useState(false);
+  const [openTicket, setOpenTicket] = useState(null);
+  const { token, currentUser } = useContext(AuthContext);
+  const API = apiURL();
+  const socket = socketIOClient(API);
+  const fetchOpenTicket = async () => {
+    try {
+      let res = await axios({
+        method: "get",
+        url: `${API}/api/tickets/open_tickets/`,
+        headers: {
+          AuthToken: token,
+        },
+      });
 
-    const makeRequest = async () => {
-        try {
-            let res = await axios({
-                method: 'post', 
-                url: `${API}/api/tickets`,
-                headers: {
-                    'AuthToken': token
-                },
-                data: {
-                    body: ""
-                }
-            })
-            socket.emit("openTicket", currentUser )
-            fetchOpenTicket()
-        } catch (error) {
-            fetchOpenTicket()
-        }
-        
+      if (res.data.openTicket.length) {
+        setIsWaitingForHelp(true);
+        setOpenTicket(res.data.openTicket[0]);
+      } else {
+        setIsWaitingForHelp(false);
+        setOpenTicket(null);
+      }
+    } catch (error) {
+      setIsWaitingForHelp(false);
+      setOpenTicket(null);
     }
-    
-    const cancelRequest = async () => {
-        try {
-            let res = await axios({
-                method: 'delete', 
-                url: `${API}/api/tickets/close_tickets/${openTicket.id}`,
-                headers: {
-                    'AuthToken': token
-                }
-            })
-            socket.emit("closeTicket", "remove ticket" )
-            fetchOpenTicket()
-        } catch (error) {
-            fetchOpenTicket()
-        }
+  };
+  useEffect(() => {
+    fetchOpenTicket();
+  }, []);
 
+  useEffect(() => {
+    socket.on("ticketClose", fetchOpenTicket);
+    return () => socket.off("ticketClose", fetchOpenTicket);
+  }, []);
+
+  const makeRequest = async () => {
+    try {
+      let res = await axios({
+        method: "post",
+        url: `${API}/api/tickets`,
+        headers: {
+          AuthToken: token,
+        },
+        data: {
+          body: "",
+        },
+      });
+      socket.emit("openTicket", currentUser);
+      fetchOpenTicket();
+    } catch (error) {
+      fetchOpenTicket();
     }
+  };
 
-    return(
-        <div className="requestContainer">
-            {isWaitingForHelp ? 
-            <button onClick={cancelRequest} className="cancelRequest request">Cancel Request</button> 
-            :
-            <button onClick={makeRequest} className="makeRequest request">Request Help</button>
-            }
-        </div>
-    )
-};
+  const cancelRequest = async () => {
+    try {
+      let res = await axios({
+        method: "delete",
+        url: `${API}/api/tickets/close_tickets/${openTicket.id}`,
+        headers: {
+          AuthToken: token,
+        },
+      });
+      socket.emit("closeTicket", "remove ticket");
+      fetchOpenTicket();
+    } catch (error) {
+      fetchOpenTicket();
+    }
+  };
+
+  return (
+    <div className="requestContainer">
+      <button
+        onClick={isWaitingForHelp ? cancelRequest : makeRequest}
+        className={
+          isWaitingForHelp ? "cancelRequest request" : "makeRequest request"
+        }
+      >
+        {isWaitingForHelp ? "Cancel Request" : "Request Help"}
+      </button>
+    </div>
+  );
+}
--- frontend/src/componets/__tests__/RequestHelp.test.js
+++ frontend/src/componets/__tests__/RequestHelp.test.js
@@ -1,38 +1,133 @@
 import React from "react";
-import { render, screen, wait } from "@testing-library/react";
+import { render, screen, wait, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import RequestHelp from "../RequestHelp";
 import { AuthContext } from "../../providers/AuthProvider";
 import axios from "axios";
+import { apiURL } from "../../util/apiURL";
 import io from "socket.io-client";
-
+import { act } from "react-dom/test-utils";
 jest.mock("socket.io-client", () => {
   const emit = jest.fn();
   const on = jest.fn();
-  const off = jest.fn()
+  const off = jest.fn();
   const socket = { emit, on, off };
   return jest.fn(() => socket);
 });
 
 
+
+const API = apiURL();
+
 jest.mock("axios");
 
-describe("RequestHelp", () => {
-  test("shows text Request Help if no previous request", async () => {
-    axios.get.mockResolvedValueOnce({
-      data: {
-        openTicket: [],
-      },
+axios.get()
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+
+test("shows text Request Help if no previous request", async () => {
+  axios.mockResolvedValue({ data: { openTicket: [] }})
+
+
+  render(
+    <AuthContext.Provider value={{ token: "1234" }}>
+      <RequestHelp />
+    </AuthContext.Provider>
+  );
+  await wait(() => expect(axios).toHaveBeenCalledTimes(1));
+  const help = await screen.findByText("Request Help");
+  expect(help).toBeInTheDocument();
+  expect(help.tagName).toBe("BUTTON");
+});
+
+test("shows text 'Cancel Request' if previous request", async () => {
+    axios.mockResolvedValueOnce({
+        data: {
+            openTicket: [{id: 1}],
+        },
+    })
+    
+    render(
+        <AuthContext.Provider value={{ token: "1234" }}>
+      <RequestHelp />
+    </AuthContext.Provider>
+  );
+  await wait(() => expect(axios).toHaveBeenCalledTimes(1));
+    const help = await screen.findByText("Cancel Request");
+    expect(help).toBeInTheDocument();
+    expect(help.tagName).toBe("BUTTON");
+});
+
+it("makes a post request to create a ticket", async () => {
+    axios.mockResolvedValueOnce({
+        data: {
+            openTicket: [],
+        },
+    })
+    
+    render(
+        <AuthContext.Provider value={{ token: "1234" }}>
+      <RequestHelp />
+    </AuthContext.Provider>
+  );
+  await wait(() => expect(axios).toHaveBeenCalledTimes(1));
+  const help = await screen.findByRole("button");
+    act(() => {
+      fireEvent.click(help);
     });
 
-    render(
-      <AuthContext.Provider value={{ token: "1234" }}>
-        <RequestHelp />
-      </AuthContext.Provider>
-    );
-     await wait(() => expect(axios).toHaveBeenCalledTimes(1)) 
-    const help = await screen.findByText("Request Help");
-     expect(help).toBeInTheDocument();
-     expect(help.tagName).toBe("BUTTON");
+  expect(axios).toHaveBeenCalledWith({
+    method: "post",
+    url: `${API}/api/tickets`,
+    headers: {
+        AuthToken: '1234'
+    }, 
+    data: {
+        body: ""
+    }
   });
-})
+});
+
+it("makes a delete request to cancel a ticket", async () => {
+    axios.mockResolvedValueOnce({
+        data: {
+            openTicket: [{id: 1}],
+        },
+    })
+    
+    render(
+        <AuthContext.Provider value={{ token: "1234" }}>
+      <RequestHelp />
+    </AuthContext.Provider>
+  );
+  await wait(() => expect(axios).toHaveBeenCalledTimes(1));
+  const help = await screen.findByRole("button");
+  act(() => {
+      fireEvent.click(help)
+
+  })
+
+  expect(axios).toHaveBeenCalledWith({
+    method: "delete",
+    url: `${API}/api/tickets/close_tickets/1`,
+    headers: {
+        AuthToken: '1234'
+    }
+  });
+});
+
+
+
+
+// it('creates a socket connection on mount', async () => {
+//      render(
+//        <AuthContext.Provider value={{ token: "1234" }}>
+//          <RequestHelp />
+//        </AuthContext.Provider>
+//      );
+
+//      expect(io.on).toHaveBeenCalledTimes(1)
+// })
