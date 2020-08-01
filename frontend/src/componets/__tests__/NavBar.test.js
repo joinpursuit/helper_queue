3c8ee3b67dac780748f10ea61f6a8a40f6e987bd
Corey Ladovsky <coreyskymusic@gmail.com>
2020-08-01 16:27:21 -0400

    complete navbar unit tests

--- frontend/src/componets/__tests__/NavBar.test.js
+++ frontend/src/componets/__tests__/NavBar.test.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { render, screen, wait, fireEvent } from "@testing-library/react";
-import { getRoles } from "@testing-library/dom";
 import "@testing-library/jest-dom/extend-expect";
 import { createMemoryHistory } from "history";
 import NavBar from "../NavBar";
@@ -8,7 +7,7 @@ import { AuthContext } from "../../providers/AuthProvider";
 import { Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "../../store";
-import JobPage from "../../features/jobs/JobPage";
+import * as firebaseFunctions from "../../util/firebaseFunctions";
 
 jest.mock("../auth/NavLogin.js", () => {
   const NavLogin = () => <div data-testid="navLogin">Login In</div>;
@@ -20,6 +19,7 @@ jest.mock("../RequestHelp.js", () => {
   return RequestHelp;
 });
 
+jest.mock("../../util/firebaseFunctions");
 
 test("home page of non user shows <NavLogin />", () => {
   const history = createMemoryHistory();
@@ -86,27 +86,47 @@ test("/jobtracker renders correct items", () => {
   expect(screen.getByPlaceholderText("Search")).toBeInTheDocument();
 });
 
+test("log out button calls to logout", () => {
+  const history = createMemoryHistory();
+  history.push("/jobtracker");
+  render(
+    <Provider store={store}>
+      <Router history={history}>
+        <AuthContext.Provider
+          value={{ token: "1234", currentUser: { email: "test@test.com" } }}
+        >
+          <NavBar />
+        </AuthContext.Provider>
+      </Router>
+    </Provider>
+  );
+  let nav = screen.getByRole("navigation");
+  expect(nav.children).toHaveLength(3);
 
-// test("/jobtracker Add Job opens modal", () => {
-//   const history = createMemoryHistory();
-//   history.push("/jobtracker");
-//   render(
-//     <Provider store={store}>
-//       <Router history={history}>
-//         <AuthContext.Provider
-//           value={{ token: "1234", currentUser: { email: "test@test.com" } }}
-//         >
-//           <NavBar />
-//           <JobPage />
-//         </AuthContext.Provider>
-//       </Router>
-//     </Provider>
-//   );
-//   let jobFormModal = screen.queryByTestId("jobFormModal");
-//   let addJob = screen.getByText("+ Add Job");
-//   expect(addJob).toBeInTheDocument();
-//   expect(jobFormModal).not.toBeInTheDocument();
-//   fireEvent.click(addJob)
-// //   jobFormModal = screen.queryByTestId("jobFormModal");
-// //   wait(() => expect(jobFormModal).toBeInTheDocument());
-// });
+  let logoutButton = screen.getByText("Log Out");
+  fireEvent.click(logoutButton);
+  expect(firebaseFunctions.logout).toHaveBeenCalledTimes(1);
+});
+
+test(" '/' and '/admin' render Home Link, Student Queue Link and Logout", () => {
+  const history = createMemoryHistory();
+  render(
+    <Provider store={store}>
+      <Router history={history}>
+        <AuthContext.Provider
+          value={{ token: "1234", currentUser: { email: "admin@admin.com" } }}
+        >
+          <NavBar />
+        </AuthContext.Provider>
+      </Router>
+    </Provider>
+  );
+  const home = screen.getByText("Home");
+  expect(home).toBeInTheDocument();
+  expect(home).toHaveAttribute("href", "/");
+
+  const queue = screen.getByText("Student Queue");
+  expect(queue).toBeInTheDocument();
+  expect(queue).toHaveAttribute("href", "/admin");
+  expect(screen.getByText("Log Out")).toBeInTheDocument();
+});
