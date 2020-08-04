4b0796a456c1b4fe632cc72a695944ffca0e06b2
Corey Ladovsky <coreyskymusic@gmail.com>
2020-08-03 20:09:53 -0400

    add index to sql tables

---
+++ backend/db/migrations/migration6.sql
@@ -0,0 +1,5 @@
+\c helper_queue;
+
+CREATE INDEX tickets_complete ON tickets(complete, owner_id);
+CREATE INDEX jobs_user_id ON jobs(user_id);
+CREATE INDEX jobs_status_timelines_job_id ON jobs_status_timelines(job_id);
