4b56ec2f57cbfd3b7c385095649af3a82747d59b
Corey Ladovsky <coreyskymusic@gmail.com>
2020-08-18 21:02:25 -0400

    get all apps for one student by email

--- backend/queries/admin.js
+++ backend/queries/admin.js
@@ -5,7 +5,7 @@ const getAllJobs = async (req, res, next) => {
     const jobs = await db.any(
       "SELECT * FROM jobs JOIN users ON users.id = jobs.user_id ORDER BY class"
     );
-    console.log(jobs)
+    console.log(jobs);
     res.json({
       jobs,
       message: "All Jobs",
@@ -18,9 +18,10 @@ const getAllJobs = async (req, res, next) => {
 const getAllJobsForClass = async (req, res, next) => {
   try {
     const jobs = await db.any(
-      "SELECT * FROM jobs JOIN users ON users.id = jobs.user_id where class = $1", req.params.class
+      "SELECT * FROM jobs JOIN users ON users.id = jobs.user_id where class = $1",
+      req.params.class
     );
-    console.log(jobs)
+    console.log(jobs);
     res.json({
       jobs,
       message: "All Jobs",
@@ -30,4 +31,31 @@ const getAllJobsForClass = async (req, res, next) => {
   }
 };
 
-module.exports = { getAllJobs, getAllJobsForClass };
+const getAllJobsForStudent = async (req, res, next) => {
+  try {
+    const jobs = await db.any(
+      "SELECT jobs.id AS job_id, jobs.company, jobs.job_title, " +
+        "jobs.post_url, jobs.salary, jobs.location, jobs.due_date, jobs.description, jobs.status, " +
+        "jobs.created_at, jobs.user_id, jobs.last_modified, users.email, users.class, " +
+        "ARRAY_AGG(jobs_status_timelines.*) AS job_status_timelines " +
+        "FROM jobs " +
+        "JOIN users ON jobs.user_id = users.id " +
+        "JOIN jobs_status_timelines ON jobs_status_timelines.job_id = jobs.id " +
+        "WHERE users.email = $1 " +
+        "GROUP BY jobs.id, users.email, users.class " +
+        "ORDER BY jobs.last_modified DESC",
+      req.params.email
+    );
+
+    res.json({
+      status: 200,
+      message: "All jobs for student",
+      email: req.params.email,
+      jobs,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getAllJobs, getAllJobsForClass, getAllJobsForStudent };
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,7 +1,14 @@
 const admin = require("express").Router();
-const { getAllJobs, getAllJobsForClass } = require("../queries/admin");
+const {
+  getAllJobs,
+  getAllJobsForClass,
+  getAllJobsForStudent,
+} = require("../queries/admin");
 
 admin.get("/alljobs", getAllJobs);
 admin.get("/alljobs/:class", getAllJobsForClass);
+
+//gets all jobs for a student by email
+admin.get("/studentjobs/:email", getAllJobsForStudent);
 
 module.exports = admin;
