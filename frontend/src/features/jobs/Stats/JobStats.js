a25f54bfff54baa462dde3980420fa643edd3a38
Corey Ladovsky <coreyskymusic@gmail.com>
2020-09-01 14:38:36 -0400

    stats page showing percentage

--- frontend/src/features/jobs/Stats/JobStats.js
+++ frontend/src/features/jobs/Stats/JobStats.js
@@ -20,19 +20,19 @@ export default () => {
     if (!jobs.length) {
       dispatch(fetchAllJobs());
     }
-     calculateStats()
-   calcCurrentStatus();
+    calculateStats();
+    calcCurrentStatus();
   }, [jobs.length]);
 
   const calculateStats = () => {
     let totalAppsThisWeek = 0;
     let oneWeekAgo = new Date(new Date().getTime() - 60 * 60 * 24 * 7 * 1000);
-    let rejections = 0; 
+    let rejections = 0;
     let phoneScreensTotal = 0;
     let techScreensTotal = 0;
     let codingChallengeTotal = 0;
-    let onsites = 0; 
-    let offers = 0; 
+    let onsites = 0;
+    let offers = 0;
     jobs.forEach((job) => {
       job.timelines.forEach((timeObj) => {
         if (
@@ -41,58 +41,119 @@ export default () => {
         ) {
           totalAppsThisWeek++;
         }
-        if(timeObj.status === "rejected") {
-            rejections++;
-        } else if(timeObj.status === "phoneScreen") {
-            phoneScreensTotal++;
-        } else if(timeObj.status === "techScreen") {
-            techScreensTotal++;
-        } else if(timeObj.status === "codingChallenge") {
-            codingChallengeTotal++;
-        } else if(timeObj.status === "onsite") {
-            onsites++;
-        } else if(timeObj.status === "offer") {
-            offers++
+        if (timeObj.status === "rejected") {
+          rejections++;
+        } else if (timeObj.status === "phoneScreen") {
+          phoneScreensTotal++;
+        } else if (timeObj.status === "techScreen") {
+          techScreensTotal++;
+        } else if (timeObj.status === "codingChallenge") {
+          codingChallengeTotal++;
+        } else if (timeObj.status === "onsite") {
+          onsites++;
+        } else if (timeObj.status === "offer") {
+          offers++;
         }
-
       });
     });
     setAppsThisWeek(totalAppsThisWeek);
     setTotalRejections(rejections);
-    setPhoneScreens(phoneScreensTotal)
-    setTechScreens(techScreensTotal)
-    setCodingChallenges(codingChallengeTotal)
-    setTotalOnsites(onsites)
-    setTotalOffers(offers)
+    setPhoneScreens(phoneScreensTotal);
+    setTechScreens(techScreensTotal);
+    setCodingChallenges(codingChallengeTotal);
+    setTotalOnsites(onsites);
+    setTotalOffers(offers);
   };
 
   const calcCurrentStatus = () => {
     let applied = 0;
     let wishlist = 0;
     jobs.forEach((job) => {
-        if (job.status === "applied" ) {
-          applied++;
-        } else if(job.status === "wishList") {
-            wishlist++;
-        }
+      if (job.status === "applied") {
+        applied++;
+      } else if (job.status === "wishList") {
+        wishlist++;
+      }
     });
-    setActiveApplied(applied)
-    setActiveWishList(wishlist)
+    setActiveApplied(applied);
+    setActiveWishList(wishlist);
   };
   return (
     <div className="adminContainer">
       <h1>Stats Page</h1>
       <div className="statsDisplay">
-        <p>Total Applications: {jobs.length}</p>
-        <p>Number Of Applications this week: {appsThisWeek}</p>
-        <p>Applications still in applied stage: {activeApplied}</p>
-        <p>Total rejections: {totalRejections}</p>
-        <p>Total phone screens ever: {phoneScreens}</p>
-        <p>Total coding challenges: {codingChallenges}</p>
-        <p>Total technical screens: {techScreens} </p>
-        <p>Current Wishlist Length: { activeWishList} </p>
-        <p>Total onsites: {totalOnsites }</p>
-        <p>Total offers: {totalOffers }</p>
+        <div className="statItem">
+          <p>Total Applications: </p>
+          <p>{jobs.length}</p>
+        </div>
+        <div className="statItem">
+          <p>Number Of Applications this week: </p>
+          <p>{appsThisWeek}</p>
+        </div>
+        <div className="statItem">
+          <p>Current Wishlist Length: </p>
+          <p>{activeWishList}</p>
+        </div>
+        <div className="statItem">
+          <p>Applications still in applied stage: </p>
+          <div>
+            <p>
+              {activeApplied} -->{" "}
+              {Math.floor((activeApplied / jobs.length) * 100)}%
+            </p>
+          </div>
+        </div>
+        <div className="statItem">
+          <p>Total rejections: </p>
+          <div>
+            <p>
+              {totalRejections} -->{" "}
+              {Math.floor((totalRejections / jobs.length) * 100)}%
+            </p>
+          </div>
+        </div>
+        <div className="statItem">
+          <p>Total phone screens ever: </p>
+          <div>
+            <p>
+              {phoneScreens}--> {Math.floor((phoneScreens / jobs.length) * 100)}
+              %
+            </p>
+          </div>
+        </div>
+        <div className="statItem">
+          <p>Total coding challenges: </p>
+          <div>
+            <p>
+              {codingChallenges} -->{" "}
+              {Math.floor((codingChallenges / jobs.length) * 100)}%
+            </p>
+          </div>
+        </div>
+        <div className="statItem">
+          <p>Total technical screens: </p>
+          <div>
+            <p>
+              {techScreens} --> {Math.floor((techScreens / jobs.length) * 100)}%
+            </p>
+          </div>
+        </div>
+
+        <div className="statItem">
+          <p>Total onsites: </p>
+          <div>
+            <p>
+              {totalOnsites} -->{" "}
+              {Math.floor((totalOnsites / jobs.length) * 100)}%
+            </p>
+          </div>
+        </div>
+        <div className="statItem">
+          <p>Total offers: </p>
+          <div>
+            <p>{totalOffers} --> {" "}{Math.floor((totalOffers / jobs.length) * 100)}%</p>
+          </div>
+        </div>
       </div>
     </div>
   );
