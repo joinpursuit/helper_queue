8c75ea4783ad7997d4d857faeefc5e3fac660160
Corey Ladovsky <coreyskymusic@gmail.com>
2021-04-06 14:55:30 -0700

    login function cypress

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -10,7 +10,15 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+Cypress.Commands.add("loginAdmin", () => {
+  cy.visit("http://localhost:3000/");
+  // must export CYPRESS_ADMIN_EMAIL AND CYPRESS_ADMIN_PASSWORD for successful tests
+  cy.get("input[placeholder='Email']").type(Cypress.env("ADMIN_EMAIL"));
+  cy.get("input[placeholder='Password']").type(Cypress.env("ADMIN_PASSWORD"));
+  cy.contains("Login").click();
+  cy.contains("Student Queue");
+  cy.contains("Log Out");
+});
 //
 //
 // -- This is a child command --
