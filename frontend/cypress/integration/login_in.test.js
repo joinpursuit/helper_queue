4f55492c1ceb499bf1773783f098e8aedc301d77
Peter Fiorentino <52140205+PeterFiorentino@users.noreply.github.com>
2021-04-20 16:48:10 -0400

    Log in tests (#15)
    * login function cypress
    
    * Writes a test to check if a student can log in
    
    Added student log in test in login_in.test.js
    
    * writes tests for a student making a request and an admin marking  the test as complete
    
    * Tests that a student can log in, make a request, admin can check request, and student can cancel request
    
    Co-authored-by: Corey Ladovsky <coreyskymusic@gmail.com>

--- frontend/cypress/integration/login_in.test.js
+++ frontend/cypress/integration/login_in.test.js
@@ -1,6 +1,4 @@
-
-
-describe("sanity check", () => {
+describe("Admin Log In", () => {
     beforeEach(() => {
         cy.visit("http://localhost:3000/");
     })
@@ -8,10 +6,10 @@ describe("sanity check", () => {
     afterEach(() => {
         indexedDB.deleteDatabase("firebaseLocalStorageDb");
     })
-    it("visits the the site localhost 3000", () => {
+    it("Visits the the site localhost 3000", () => {
         cy.contains("Helpful Resources And Links");
     })
-    it("logs in an admin", () => {
+    it("Logs in an admin", () => {
         // must export CYPRESS_ADMIN_EMAIL AND CYPRESS_ADMIN_PASSWORD for successful tests
         cy.get("input[placeholder='Email']").type(Cypress.env("ADMIN_EMAIL"));
         cy.get("input[placeholder='Password']").type(Cypress.env("ADMIN_PASSWORD"))
@@ -19,5 +17,165 @@ describe("sanity check", () => {
         
         cy.contains("Student Queue")
         cy.contains("Log Out")
+    })
+})
+
+
+describe("Student Log In", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000/");
+    })
+    
+    afterEach(() => {
+        indexedDB.deleteDatabase("firebaseLocalStorageDb");
+    })
+    it("Visits the the site localhost 3000", () => {
+        cy.contains("Helpful Resources And Links");
+    })
+    it("Logs in a student", () => {
+        // must export CYPRESS_ADMIN_EMAIL AND CYPRESS_ADMIN_PASSWORD for successful tests
+        cy.get("input[placeholder='Email']").type(Cypress.env("STUDENT_EMAIL"));
+        cy.get("input[placeholder='Password']").type(Cypress.env("STUDENT_PASSWORD"))
+        cy.contains("Login").click();
+        
+        cy.contains("Job Tracker")
+        cy.contains("Coding Practice")
+        cy.contains("Documentation")
+        cy.contains("Curriculum")
+        cy.contains("Log Out")
+    })
+})
+
+describe("Student Makes A Help Request", ()  => {
+
+    
+    
+    afterEach(() => {
+        indexedDB.deleteDatabase("firebaseLocalStorage")
+    })
+    
+    it("Logs in a student", () => {
+        cy.visit("http://localhost:3000/")
+        // must export CYPRESS_ADMIN_EMAIL AND CYPRESS_ADMIN_PASSWORD for successful tests
+        cy.get("input[placeholder='Email']").type(Cypress.env("STUDENT_EMAIL"));
+        cy.get("input[placeholder='Password']").type(Cypress.env("STUDENT_PASSWORD"))
+        cy.contains("Login").click();
+        
+        cy.contains("Job Tracker")
+        cy.contains("Coding Practice")
+        cy.contains("Documentation")
+        cy.contains("Curriculum")
+    })
+     
+    it("Requests help", () => {
+        cy.contains("Request Help").click()
+        cy.contains("Cancel Request")
+        
+    })
+
+    it("Logs a student out", () => { 
+        cy.contains("Log Out").click()
+    })
+    
+    it("Logs in as admin", () => {
+        cy.get("input[placeholder='Email']").type(Cypress.env("ADMIN_EMAIL"));
+        cy.get("input[placeholder='Password']").type(Cypress.env("ADMIN_PASSWORD"))
+        cy.contains("Login").click();
+    })
+
+    it("Checks the Student Queue for the request made", () => {
+        cy.contains("Student Queue").click()
+
+        cy.contains(Cypress.env("STUDENT_EMAIL"));
+    })
+
+    it("Marks the request as complete", () => {
+        cy.contains("Complete").click()
+        cy.contains("No Students Waiting")
+    })
+        
+    it("Logs an admin out", () => { 
+        cy.contains("Log Out").click()
+    })
+
+})
+
+describe("Student can cancel a help request", () => {
+    
+    afterEach(() => {
+        indexedDB.deleteDatabase("firebaseLocalStorage")
+    })
+
+    it("Logs in a student", () => {
+        cy.visit("http://localhost:3000/")
+        // must export CYPRESS_ADMIN_EMAIL AND CYPRESS_ADMIN_PASSWORD for successful tests
+        cy.get("input[placeholder='Email']").type(Cypress.env("STUDENT_EMAIL"));
+        cy.get("input[placeholder='Password']").type(Cypress.env("STUDENT_PASSWORD"))
+        cy.contains("Login").click();
+        
+        cy.contains("Job Tracker")
+        cy.contains("Coding Practice")
+        cy.contains("Documentation")
+        cy.contains("Curriculum")
+    })
+     
+    it("Requests help", () => {
+        cy.contains("Request Help").click()
+        cy.contains("Cancel Request")
+        
+    })
+
+    it("Logs a student out", () => { 
+        cy.contains("Log Out").click()
+    })
+    
+    it("Logs in as admin", () => {
+        cy.get("input[placeholder='Email']").type(Cypress.env("ADMIN_EMAIL"));
+        cy.get("input[placeholder='Password']").type(Cypress.env("ADMIN_PASSWORD"))
+        cy.contains("Login").click();
+    })
+
+    it("Checks the Student Queue for the request made", () => {
+        cy.contains("Student Queue").click()
+
+        cy.contains(Cypress.env("STUDENT_EMAIL"));
+    })
+
+    it("Logs an admin out", () => { 
+        cy.contains("Log Out").click()
+    })
+
+    it("Logs in a student", () => {
+        cy.visit("http://localhost:3000/")
+        // must export CYPRESS_ADMIN_EMAIL AND CYPRESS_ADMIN_PASSWORD for successful tests
+        cy.get("input[placeholder='Email']").type(Cypress.env("STUDENT_EMAIL"));
+        cy.get("input[placeholder='Password']").type(Cypress.env("STUDENT_PASSWORD"))
+        cy.contains("Login").click();
+        
+        cy.contains("Job Tracker")
+        cy.contains("Coding Practice")
+        cy.contains("Documentation")
+        cy.contains("Curriculum")
+    })
+
+    it("Cancels help request", () => {
+        cy.contains("Cancel Request").click()
+        cy.contains("Request Help")
+    })
+
+    it("Logs a student out", () => { 
+        cy.contains("Log Out").click()
+    })
+
+    it("Logs in as admin", () => {
+        cy.get("input[placeholder='Email']").type(Cypress.env("ADMIN_EMAIL"));
+        cy.get("input[placeholder='Password']").type(Cypress.env("ADMIN_PASSWORD"))
+        cy.contains("Login").click();
+    })
+
+    it("Has no requests waiting", () => {
+        cy.contains("Student Queue").click()
+        cy.contains("No Students Waiting")
+
     })
 })
